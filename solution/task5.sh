2328d56a4cb2e181eea480e08097802079cfadc7
Danil Zhukov <zhukov.danila2004@yandex.ru>
2024-04-27 07:23:17 +0700

    fixed 5 task

--- solution/task5.sh
+++ solution/task5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -r 's/([[:alpha:]]+) ([[:alpha:]]+)( [[:alpha:]]+)?,([0-9]+)/\2 \1 (\4)/' data/students.csv > data/students.txt
+sed -r 's/([^[:space:]]+) ([^[:space:]]+)( [[:alpha:]]+)?,([0-9]+)/\2 \1 (\4)/' data/students.csv > data/students.txt
